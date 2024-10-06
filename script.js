94e3d84e1f6298e228831011ac28d1226b0aed65
@maptaul <maptaulislam1@gmail.com>
2024-10-06 23:24:10 +0600

    af

--- script.js
+++ script.js
@@ -73,7 +73,9 @@ class="flex justify-between *: font-bold [&>not
 </div>
 </div>
 <div class="opacity-100">
-<button id="addToList" onclick="markAsRead()"
+<button id="addToList" onclick="markAsRead('${post.description}','${
+      post.view_count
+    }')"
 data-post='$(JSON.stringify(post))' class="addToList btn btn-circle bg-green-500 btn-sm">
 <i class="fa-solid fa-envelope-open text-white"></i>
 </button>
@@ -85,6 +87,23 @@ data-post='$(JSON.stringify(post))' class="addToList btn btn-circle bg-green-500
     postContainer.appendChild(div);
   });
 };
+
+const markAsRead = (description, view_count) => {
+  markAsReadContainer = document.getElementById("markAsReadContainer");
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="flex justify-between p-2 lg:p-3bg-white rounded-2xl items-center gap-3">
+<div class="lg:w-4/5 w-11/12">
+<p>${description}</p>
+</div>
+<div class="lg:w-1/5 w-4/12 flex justify-end">
+<p><i class="fa-regular fa-eye"></i> ${view_count}</p>
+</div>
+</div>
+  `;
+  markAsReadContainer.appendChild(div);
+};
+
 loadAllPost();
 
 const handleSearchByCategory = () => {
